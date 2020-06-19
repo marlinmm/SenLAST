618c2ba35ea5edf828012c7bf518ee678d8219c7
marlinmm <marlinmueller.mm@gmail.com>
2020-06-19 17:44:29 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+directory = os.path.abspath(os.path.dirname(__file__))
+if sys.version_info >= (3, 0):
+    with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+else:
+    with open(os.path.join(directory, 'README.md')) as f:
+        long_description = f.read()
+
+setup(name='BanDiTS',
+      packages=find_packages(),
+      include_package_data=True,
+      setup_requires=['setuptools_scm'],
+      use_scm_version=True,
+      description='....', # add description
+      classifiers=[
+          'License :: FSF Approved :: MIT License',
+          'Operating System :: Microsoft :: Windows',
+          'Programming Language :: Python',
+      ],
+      install_requires=['...', # add requieremtns
+                        ],
+      python_requires='>=3.6.0',
+      url='https://github.com/marlinmm/SenLAST.git',
+      author='Marlin Mueller', #'Jonas Ziemer'  # how to add second author?
+      author_email='marlin.markus.mueller@uni-jena.de',
+      license='MIT',
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown')
