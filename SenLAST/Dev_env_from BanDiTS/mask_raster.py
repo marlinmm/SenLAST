ba40109a79db2fa1591de4428542558713ac00d4
marlinmm <marlinmueller.mm@gmail.com>
2020-06-22 13:12:02 +0200

    added a needed +1

--- SenLAST/Dev_env_from BanDiTS/mask_raster.py
+++ SenLAST/Dev_env_from BanDiTS/mask_raster.py
@@ -16,9 +16,9 @@ def mask_raster_test():
 
     for h in range(0,19):
         ## Shapefile Marlin:
-        # shapefile = fiona.open("F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box_reprojected.shp", "r")
+        shapefile = fiona.open("F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box_reprojected.shp", "r")
         ## Shapefile Jonas:
-        shapefile = fiona.open("C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp")
+        #shapefile = fiona.open("C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp")
 
         shapes = [feature["geometry"] for feature in shapefile]
         shape_list.append(shapes)
@@ -28,11 +28,11 @@ def mask_raster_test():
 
     list_means = []
 
-    for i in range(0, len(shape_list)):
+    for i in range(0, len(shape_list)+1):
         ## Tiff-File Marlin:
-        # src1 = rio.open("F:/GEO411_data/MODIS_R_dir/Downloaded_HDFs/GeoTIFF/Thuringia/scaled/MOD11A1.A2018184.h18v03.006_LST_Day_1km_latlon_wgs84_Thuringia_celsius.tif")
+        src1 = rio.open("F:/GEO411_data/MODIS_R_dir/Downloaded_HDFs/GeoTIFF/Thuringia/scaled/MOD11A1.A2018184.h18v03.006_LST_Day_1km_latlon_wgs84_Thuringia_celsius.tif")
         ## Tiff-File Jonas:
-        src1 = rio.open("F:/411/LST/GeoTIFF/Thuringia/scaled/MOD11A1.A2020153.h18v03.006_LST_Day_1km_latlon_wgs84_Thuringia_celsius.tif")
+        #src1 = rio.open("F:/411/LST/GeoTIFF/Thuringia/scaled/MOD11A1.A2020153.h18v03.006_LST_Day_1km_latlon_wgs84_Thuringia_celsius.tif")
         out_image1, out_transform1 = rasterio.mask.mask(src1, [shape_list[0][i]], all_touched=0, crop=True, nodata= np.nan)
         ras_meta1 = src1.profile
         ras_meta1.update({"driver": "GTiff",
@@ -45,6 +45,7 @@ def mask_raster_test():
         # print(out_image1)
 
     print(list_means)
+    print(len(list_means))
 
 #### activate for testing this file standalone ####
 mask_raster_test()
