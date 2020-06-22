43f93e283555a82fdb4b41391f5d0bd38a86051c
marlinmm <marlinmueller.mm@gmail.com>
2020-06-22 16:54:45 +0200

    added Marlin Path

--- SenLAST/Dev_env_from BanDiTS/extract.py
+++ SenLAST/Dev_env_from BanDiTS/extract.py
@@ -1,6 +1,11 @@
 import os
 
+## Jonas Folder:
 directory = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Sentinel 3 Daten Uni Jena/S3_Daten-20200622T143538Z-001/S3_Daten"
+
+## Marlin Folder:
+directory = "F:/GEO411_data/Sentinel_Daten"
+
 new_list = []
 for filename in os.listdir(directory):
     if filename.endswith(".tif"):
