e219869779502d1a3a3e149c85d950f78f2e6e8d
jziemer1996 <jz199623@gmail.com>
2020-06-22 16:46:33 +0200

    added directory tif listdir

--- SenLAST/Dev_env_from BanDiTS/extract.py
+++ SenLAST/Dev_env_from BanDiTS/extract.py
@@ -1,1 +1,8 @@
+import os
 
+directory = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Sentinel 3 Daten Uni Jena/S3_Daten-20200622T143538Z-001/S3_Daten"
+for filename in os.listdir(directory):
+    if filename.endswith(".tif"):
+        print(os.path.join(directory, filename))
+    else:
+        continue
