fd70cb3421c36d321224af0fccacedab36f692d3
marlinmm <marlinmueller.mm@gmail.com>
2020-06-22 16:57:08 +0200

    added function name

--- SenLAST/Dev_env_from BanDiTS/extract.py
+++ SenLAST/Dev_env_from BanDiTS/extract.py
@@ -1,16 +1,17 @@
 import os
 
-## Jonas Folder:
-directory = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Sentinel 3 Daten Uni Jena/S3_Daten-20200622T143538Z-001/S3_Daten"
+def extract_files_to_list():
+    ## Jonas Folder:
+    directory = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Sentinel 3 Daten Uni Jena/S3_Daten-20200622T143538Z-001/S3_Daten"
 
-## Marlin Folder:
-directory = "F:/GEO411_data/Sentinel_Daten"
+    ## Marlin Folder:
+    directory = "F:/GEO411_data/Sentinel_Daten"
 
-new_list = []
-for filename in os.listdir(directory):
-    if filename.endswith(".tif"):
-        new_list.append(os.path.join(directory, filename))
-    else:
-        continue
+    new_list = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".tif"):
+            new_list.append(os.path.join(directory, filename))
+        else:
+            continue
 
-print(new_list)
+    print(new_list)
