9549f69776f51cb67e57da7b223a31f46572c456
marlinmm <marlinmueller.mm@gmail.com>
2020-06-25 18:39:52 +0200

    cleaned code and some functions

--- SenLAST/extract.py
+++ SenLAST/extract.py
@@ -6,13 +6,27 @@ import numpy as np
 import fiona
 import shutil
 
+# Kurze Info zu "i in range()":
+## https://medium.com/better-programming/stop-using-range-in-your-python-for-loops-53c04593f936
+
 start_time = datetime.now()
 
+### ----- TIFF Data ----- ###
 ## Jonas Folder:
 # directory = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten"
-
 ## Marlin Folder:
 directory = "F:/GEO411_data/Sentinel_Daten"
+
+### ----- Shapefile Data ----- ###
+## Shapefile Marlin:
+shapefolder = "F:/GEO411_data/Daten_Sandra/new/"
+shapefile = "Stationen_Thüringen_Umland_3x3box.shp"
+## Shapefile Jonas:
+# shapefolder = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/"
+#shapefile = "Stationen_Thüringen_Umland_3x3box.shp"
+
+shape_path = shapefolder + shapefile
+
 
 def extract_files_to_list(path_to_folder):
     """
@@ -29,23 +43,15 @@ def extract_files_to_list(path_to_folder):
     return new_list
 
 ### this function needs to be generalized ###
-def import_polygons():
+def import_polygons(shape_path):
     """
     imports the 3x3km polygons of the DWD weather stations
     :return:
     """
     shape_list = []
-
-    for h in range(0,19):
-        ## Shapefile Marlin:
-        shapefolder = "F:/GEO411_data/Daten_Sandra/new/"
-        ## Shapefile Jonas:
-        # shapefolder = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/"
-
-        inputshape = "Stationen_Thüringen_Umland_3x3box.shp"
-        shapefile = fiona.open(shapefolder+inputshape, "r")
-
-        shapes = [feature["geometry"] for feature in shapefile]
+    acitve_shapefile = fiona.open(shape_path, "r")
+    for i in range(0,len(list(acitve_shapefile))-1):
+        shapes = [feature["geometry"] for feature in acitve_shapefile]
         shape_list.append(shapes)
     return shape_list
 
@@ -55,21 +61,18 @@ def eliminate_nanoverlap():
     eliminates the scenes which would'nt match with all weather stations
     :return:
     """
-    import_list = import_polygons()
+    import_list = import_polygons(shape_path=shape_path)
     file_list = extract_files_to_list(path_to_folder=directory)
     tifs_selected = directory + "/selected"
     if os.path.exists(tifs_selected):
         shutil.rmtree(tifs_selected)
     os.mkdir(directory + "/selected")
 
-    for i in range(0, len(file_list)):
-        # Select between MODIS and SENTINEL
+    for i, files in enumerate(file_list):
         src1 = rio.open(file_list[i])
-        # src1 = rio.open(Modis_folder + Modis_file)
         try:
-            for j in range(0, len(import_list)+1):
-                out_image1, out_transform1 = rio.mask.mask(src1, [import_list[0][j]], all_touched=0, crop=True,
-                                                           nodata=np.nan)
+            for j, polygons in enumerate(import_list):
+                rio.mask.mask(src1, [import_list[0][j]], all_touched=0, crop=True, nodata=np.nan)
             shutil.copy(file_list[i], tifs_selected)
         except ValueError:
             pass
@@ -90,23 +93,23 @@ def eliminate_cloudy_data():
     selected_tifs = extract_files_to_list(path_to_folder=directory + "/selected")
 
     # import polygons from shapefile
-    import_list = import_polygons()
+    import_list = import_polygons(shape_path=shape_path)
 
     # loop through all .tif files in folder
-    for tif in range(0, len(selected_tifs)):
-        src1 = rio.open(selected_tifs[tif])
+    for i, tifs in enumerate(selected_tifs):
+        src1 = rio.open(selected_tifs[i])
         bool_list = []
         flag = 0   #set boolean flag to 0
 
         # loop through all weatherstations for each .tif file
-        for polygons in range(0, len(import_list)+1):
-            out_image1, out_transform1 = rio.mask.mask(src1, [import_list[0][polygons]], all_touched=1, crop=True,
+        for j, polygons in enumerate(import_list):
+            out_image1, out_transform1 = rio.mask.mask(src1, [import_list[0][j]], all_touched=1, crop=True,
                                                        nodata=np.nan)
 
             # extract minimal temperature at all stations for each .tif file to eliminate error values
             min_temp = np.min(out_image1[0])
 
-            # create boolean values for error values and store in list
+            # create boolean values for error values and store in list (-40° is lowest remotely realistic temperature)
             if min_temp < -40:
                 bool_list.append(False)
             else:
@@ -118,7 +121,7 @@ def eliminate_cloudy_data():
                 flag = 1
                 break
         if flag == 0:
-            shutil.copy(selected_tifs[tif], cloud_free)
+            shutil.copy(selected_tifs[j], cloud_free)
 
 eliminate_cloudy_data()
 
