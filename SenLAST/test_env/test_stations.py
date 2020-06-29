abdd8ddf4dfa68ce852e624cb719bcc5468261a0
marlinmm <marlinmueller.mm@gmail.com>
2020-06-29 11:27:25 +0200

    updated some more stuff

--- SenLAST/test_env/test_stations.py
+++ SenLAST/test_env/test_stations.py
@@ -7,28 +7,41 @@ import numpy as np
 import fiona
 import shutil
 
-# Kurze Info zu "i in range()":
-## https://medium.com/better-programming/stop-using-range-in-your-python-for-loops-53c04593f936
-
 
 start_time = datetime.now()
 
 ### ----- TIFF Data ----- ###
 ## Jonas Folder:
-directory = "F:/411/LST/GeoTIFF/Thuringia/scaled"
+# directory = "F:/411/LST/GeoTIFF/Thuringia/scaled"
 ## Marlin Folder:
-# directory = "F:/GEO411_data/MODIS_Daten/MODIS_download"
+directory = "F:/GEO411_data/MODIS_Daten/MODIS_download"
 
 ### ----- Shapefile Data ----- ###
 ## Shapefile Marlin:
-# shapefolder = "F:/GEO411_data/Daten_Sandra/new/"
-# shapefile = "Stationen_Thüringen_Umland_3x3box_reprojected.shp"
-## Shapefile Jonas:
-shapefolder = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/"
+shapefolder = "F:/GEO411_data/Daten_Sandra/new/"
 shapefile = "Stationen_Thüringen_Umland_3x3box_reprojected.shp"
+## Shapefile Jonas:
+#shapefolder = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/"
+#shapefile = "Stationen_Thüringen_Umland_3x3box_reprojected.shp"
 
 shape_path = shapefolder + shapefile
 
+
+def extract_files_to_list(path_to_folder):
+    """
+    finds all .tif-files in the corresponding directory
+    :return:
+    """
+    new_list = []
+
+    for filename in os.listdir(path_to_folder):
+        if filename.endswith(".tif"):
+            new_list.append(os.path.join(path_to_folder, filename))
+        else:
+            continue
+    return new_list
+
+### this function needs to be generalized ###
 def import_polygons(shape_path):
     """
     imports the 3x3km polygons of the DWD weather stations
@@ -42,36 +55,42 @@ def import_polygons(shape_path):
     return shape_list
 
 
-
 def find_best_stations():
-    # selected_tifs = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free"
-    selected_tifs = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free"
+    """
+    eliminates the scenes which are not cloud_free
+    :return:
+    """
+    # create new cloud_free directory, overwrite if already exits
+    selected_tifs = extract_files_to_list(path_to_folder=directory)
+
+    # import polygons from shapefile
     import_list = import_polygons(shape_path=shape_path)
     station_list = []
+    # loop through all .tif files in folder
     for i, tifs in enumerate(selected_tifs):
         src1 = rio.open(selected_tifs[i])
         bool_list = []
         flag = 0   #set boolean flag to 0
 
         # loop through all weatherstations for each .tif file
+        single_station_list = []
         for j, polygons in enumerate(import_list):
             out_image1, out_transform1 = rio.mask.mask(src1, [import_list[0][j]], all_touched=1, crop=True,
                                                        nodata=np.nan)
 
             # extract nanmean temperature at all stations for each .tif file to eliminate error values
-
             mean_temp = np.nanmean(out_image1[0])
-            station_list.append(mean_temp)
+
+            single_station_list.append(mean_temp)
 
             # create boolean values for error values and store in list (-40° is lowest remotely realistic temperature)
             if mean_temp < -40 or np.isnan(mean_temp) == True:
                 bool_list.append(False)
             else:
                 bool_list.append(True)
+
+        station_list.append(single_station_list)
+
     print(station_list)
-
-
-
-
+    print(len(station_list))
 find_best_stations()
-
