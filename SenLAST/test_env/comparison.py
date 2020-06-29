684b3a55038d7c172d7bcbf761353f9dbfc742b9
marlinmm <marlinmueller.mm@gmail.com>
2020-06-29 17:40:37 +0200

    corrected minor stuff in reconversion func

--- SenLAST/test_env/comparison.py
+++ SenLAST/test_env/comparison.py
@@ -9,27 +9,27 @@ start_time = datetime.now()
 
 ### ----- TIFF Data ----- ###
 ## Jonas Folder:
-directory1 = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten/selected/cloud_free"
+# directory1 = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten/selected/cloud_free"
 # directory1 = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Sentinel 3 Daten Uni Jena/Sentinel 3 Daten Uni Jena/2019/06_2019_Juni/S3A"
-directory2 = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free"
+# directory2 = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free"
 ## Marlin Folder:
-# directory1 = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free"
-# directory2 = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free"
+directory1 = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free"
+directory2 = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free"
 
 ### ----- TIFF Data ----- ###
 ## Jonas Folder:
 # directory = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten"
-directory = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free"
+# directory = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free"
 ## Marlin Folder:
-# directory = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free/"
+directory = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free/"
 
 ### ----- Shapefile Data ----- ###
 ## Shapefile Marlin:
-# shapefolder = "F:/GEO411_data/Daten_Sandra/new/"
-# shapefile = "Stationen_Thüringen_Umland_3x3box.shp"
-## Shapefile Jonas:
-shapefolder = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/"
+shapefolder = "F:/GEO411_data/Daten_Sandra/new/"
 shapefile = "Stationen_Thüringen_Umland_3x3box.shp"
+## Shapefile Jonas:
+# shapefolder = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/"
+# shapefile = "Stationen_Thüringen_Umland_3x3box.shp"
 
 shape_path = shapefolder + shapefile
 
@@ -104,8 +104,8 @@ extract_MODIS_timestamp_all_years(directory2)
 ## For more information see: https://stackoverflow.com/questions/52464978/how-do-i-print-elements-of-one-list-that-are-in-another-list
 c = set(SENTINEL_timestamp_list) & set(MODIS_timestamp_list)
 overlap_list = list(c)
-print("SENTINEL/MODIS SCENES WITH TEMPORAL OVERLAP:")
-print(overlap_list)
+#print("SENTINEL/MODIS SCENES WITH TEMPORAL OVERLAP:")
+#print(overlap_list)
 # print('\n'.join(str(i) for i in c))
 
 
@@ -140,13 +140,14 @@ def select_SENTINEL_scenes():
 
 def reconversion():
     overlap_doy_list = []
+    year_list = [2018, 2019, 2020]
     for a, doy in enumerate(overlap_list):
         modis_date = pd.to_datetime(overlap_list[a], format='%Y-%m-%d')
-        # print(modis_date)
-        new_year_day = pd.Timestamp(year=2019, month=1, day=1)
+        modis_date_year = str(modis_date)[0:4]
+        new_year_day = pd.Timestamp(year=int(modis_date_year), month=1, day=1)
         # print(new_year_day)
         overlap_doy_list.append((modis_date - new_year_day).days + 1)
-        print(overlap_doy_list)
+    print(overlap_doy_list)
 
 reconversion()
 
