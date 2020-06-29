9ef428d6ed05ebb371bd4d07e0b1c7351616288f
marlinmm <marlinmueller.mm@gmail.com>
2020-06-29 09:26:29 +0200

    add function to generate list for all years

--- SenLAST/test_env/comparison.py
+++ SenLAST/test_env/comparison.py
@@ -8,11 +8,11 @@ start_time = datetime.now()
 ### ----- TIFF Data ----- ###
 ## Jonas Folder:
 # directory = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten"
-directory1 = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Sentinel 3 Daten Uni Jena/Sentinel 3 Daten Uni Jena/2019/06_2019_Juni/S3A"
-directory2 = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free"
+#directory1 = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Sentinel 3 Daten Uni Jena/Sentinel 3 Daten Uni Jena/2019/06_2019_Juni/S3A"
+#directory2 = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free"
 ## Marlin Folder:
-# directory1 = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free"
-# directory2 = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free"
+directory1 = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free"
+directory2 = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free"
 
 SENTINEL_timestamp_list = []
 MODIS_doy_list = []
@@ -52,7 +52,32 @@ def extract_MODIS_timestamp(directory2, year):
     # print(MODIS_doy_list)
     print(MODIS_timestamp_list)
 
-extract_MODIS_timestamp(directory2, 2019)
+
+#extract_MODIS_timestamp(directory2, 2020)
+
+
+def extract_MODIS_timestamp_all_years(directory2):
+    """
+    extracts the acquisition date of MODIS scenes into a new list
+    ## for more information see: https://stackoverflow.com/questions/2427555/python-question-year-and-day-of-year-to-date
+    :return:
+    """
+    years = [2018, 2019, 2020]
+    for i, year in enumerate (years):
+        for filename in os.listdir(directory2):
+            timestamp = filename[13:16]
+            MODIS_doy_list.append(os.path.join(timestamp))
+            for doy in range(len(MODIS_doy_list)):
+                doy = date.fromordinal(date(years[i], 1, 1).toordinal() + int(timestamp)-1)
+                doy = str(doy)
+            # print(str(doy))
+            MODIS_timestamp_list.append(doy)
+
+    # print(MODIS_doy_list)
+    print(MODIS_timestamp_list)
+
+
+extract_MODIS_timestamp_all_years(directory2)
 
 ## Compare the temporal overlap between SENTINEL and MODIS Data
 ## For more information see: https://stackoverflow.com/questions/52464978/how-do-i-print-elements-of-one-list-that-are-in-another-list
