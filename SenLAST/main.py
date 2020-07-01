0ed1215ca67a4cda09f48418181a05758f40af8e
jziemer1996 <jz199623@gmail.com>
2020-07-01 16:42:58 +0200

    added input folder paths

--- SenLAST/main.py
+++ SenLAST/main.py
@@ -1,5 +1,6 @@
 ##############################     IMPORT OF REQUIRED MODULES    ###################################
 from datetime import datetime
+from SenLAST.comparison import *
 from SenLAST.data_preprocessing import eliminate_MODIS_cloudy_data, eliminate_SENTINEL_cloudy_data
 from SenLAST.comparison import select_MODIS_scenes, select_SENTINEL_scenes
 
@@ -12,32 +13,33 @@ def main():
     #### ----- TIFF Data ----- ####
     ### Jonas base Folder:
     ## MODIS:
-    # MODIS_directory = "F:/411/LST/GeoTIFF/Thuringia/scaled"
+    MODIS_directory = "F:/Modis/MODIS_all"
     ## Sentinel:
-    # Sentinel_directory = ""
+    Sentinel_directory = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten/selected/cloud_free/final_sentinel_selected"
 
     ### Shapefiles:
     ## MODIS Shapefile:
-    # MODIS_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
+    MODIS_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
     ## Sentinel Shapefile:
-    # Sentinel_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box.shp"
+    Sentinel_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box.shp"
 
 
     ### Marlin base Folder:
     ## MODIS:
-    MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download"
+    # MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download"
     ## Sentinel:
-    Sentinel_directory = "F:/GEO411_data/Sentinel_Daten"
+    # Sentinel_directory = "F:/GEO411_data/Sentinel_Daten"
 
     ### Shapefiles:
     ## MODIS Shapefile:
-    MODIS_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
+    # MODIS_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
     ## Sentinel Shapefile:
-    Sentinel_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box.shp"
+    # Sentinel_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box.shp"
 
     ####################### USER-DEPENDENT FILTER-FUNCTIONS TO BE USED #######################
 
-    ### extract correct Sentinel and MODIS data from all downloaded data ###
+    ### Extract correct Sentinel and MODIS data from all downloaded data ###
+
     # eliminate_SENTINEL_cloudy_data(sen_directory=Sentinel_directory, shape_path=Sentinel_shapefile)
     # eliminate_MODIS_cloudy_data(mod_directory=MODIS_directory, shape_path=MODIS_shapefile)
 
