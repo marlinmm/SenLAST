d318d3b7913672b25ab5eb7264c444179da63bdf
jziemer1996 <jz199623@gmail.com>
2020-07-02 12:03:11 +0200

    added compare_timestamp function

--- SenLAST/comparison.py
+++ SenLAST/comparison.py
@@ -3,6 +3,7 @@ from datetime import date
 import shutil
 import pandas as pd
 from SenLAST.base_information import extract_files_to_list
+import numpy as np
 
 ######################## SENTINEL DATE & TIME EXTRACTION ########################
 
@@ -88,39 +89,87 @@ def extract_MODIS_timestamp(mod_directory):
 
 ######################## COMPARISON FUNCTIONS ##########################
 
-def compare(mod_directory, sen_directory):
-    ## Compare the temporal overlap between SENTINEL and MODIS Data
-    ## For more information see: https://stackoverflow.com/questions/52464978/how-do-i-print-elements-of-one-list-that-are-in-another-list
-    new_sen_directory = sen_directory + "/selected/cloud_free"
-    new_mod_directory = mod_directory + "/cloud_free"
+def compare_date(mod_directory, sen_directory):
+    """
+    Compares the temporal (date) overlap between SENTINEL and MODIS Data
+    - For more information see: https://stackoverflow.com/questions/52464978/how-do-i-print-elements-of-one-list-that-are-in-another-list
+    """
+    date_sen_directory = sen_directory + "/selected/cloud_free"
+    date_mod_directory = mod_directory + "/cloud_free"
 
-    sentinel_data = extract_SENTINEL_date(sen_directory=new_sen_directory)
-    modis_data = extract_MODIS_date(mod_directory=new_mod_directory)
+    sentinel_date_data = extract_SENTINEL_date(sen_directory=date_sen_directory)
+    modis_date_data = extract_MODIS_date(mod_directory=date_mod_directory)
 
-    c = set(sentinel_data) & set(modis_data)
-    overlap_list = list(c)
-    return overlap_list
+    c = set(sentinel_date_data) & set(modis_date_data)
+    overlap_date_list = list(c)
+    print(overlap_date_list)
+    # return overlap_date_list
+
+def compare_timestamp(mod_directory, sen_directory):
+    """
+    Compares the temporal (time) overlap between SENTINEL and MODIS Data
+    - For more information see: https://stackoverflow.com/questions/52464978/how-do-i-print-elements-of-one-list-that-are-in-another-list
+    """
+    time_sen_directory = sen_directory + "/selected/cloud_free"
+    time_mod_directory = mod_directory + "/cloud_free"
+
+    sentinel_timestamp_data = extract_SENTINEL_timestamp(sen_directory=time_sen_directory)
+    modis_timestamp_data = extract_MODIS_timestamp(mod_directory=time_mod_directory)
+
+    overlap_time_list = []
+
+    for x in sentinel_timestamp_data:
+        for y in modis_timestamp_data:
+            xa = int(x)+45
+            xb = int(x)-45
+            y =int(y)
+
+            if y <= xa and y >= xb:
+                overlap_time_list.append("True")
+            else:
+                overlap_time_list.append("False")
+    print(overlap_time_list)
+
+
+# def select_SENTINEL_scenes(mod_directory, sen_directory):
+#     new_sen_directory = sen_directory + "/selected/cloud_free"
+#     new_list = extract_files_to_list(path_to_folder=new_sen_directory)
+#     overlap_date_list = compare_date(mod_directory=mod_directory, sen_directory=sen_directory)
+#     final_tifs_selected = new_sen_directory + "/final_sentinel_selected/"
+#     if os.path.exists(final_tifs_selected):
+#         shutil.rmtree(final_tifs_selected)
+#     os.mkdir(new_sen_directory + "/final_sentinel_selected/")
+#     for i, element in enumerate(overlap_date_list):
+#         for j, tiff in enumerate(new_list):
+#             if overlap_date_list[i] in new_list[j]:
+#                 shutil.copy(new_list[j], final_tifs_selected)
 
 
 def select_SENTINEL_scenes(mod_directory, sen_directory):
     new_sen_directory = sen_directory + "/selected/cloud_free"
     new_list = extract_files_to_list(path_to_folder=new_sen_directory)
-    overlap_list = compare(mod_directory=mod_directory, sen_directory=sen_directory)
+    overlap_date_list = compare_date(mod_directory=mod_directory, sen_directory=sen_directory)
+    overlap_time_list = compare_timestamp(mod_directory=mod_directory, sen_directory=sen_directory)
     final_tifs_selected = new_sen_directory + "/final_sentinel_selected/"
     if os.path.exists(final_tifs_selected):
         shutil.rmtree(final_tifs_selected)
     os.mkdir(new_sen_directory + "/final_sentinel_selected/")
-    for i, element in enumerate(overlap_list):
+    for i, element in enumerate(overlap_date_list):
         for j, tiff in enumerate(new_list):
-            if overlap_list[i] in new_list[j]:
-                shutil.copy(new_list[j], final_tifs_selected)
+            if overlap_date_list[i] in new_list[j]:
+                for k, booltime in enumerate(overlap_time_list):
+                    if overlap_time_list[k] == True:
+                        shutil.copy(new_list[j], final_tifs_selected)
 
+# funktioniert nur für den Fall, wenn overlap_time_list komplett false ist
+# funktioniert auch noch nicht wenn es unterschiedliche Tage sind - denk ich zumindest
+# ab hier: nachfolgenden MODIS-Funktionen sind noch nicht auf neue Methodik angepasst
 
 def reconversion(mod_directory, sen_directory):
-    overlap_list = compare(mod_directory=mod_directory, sen_directory=sen_directory)
+    overlap_date_list = compare_date(mod_directory=mod_directory, sen_directory=sen_directory)
     overlap_doy_list = []
-    for a, doy in enumerate(overlap_list):
-        modis_date = pd.to_datetime(overlap_list[a], format='%Y-%m-%d')
+    for a, doy in enumerate(overlap_date_list):
+        modis_date = pd.to_datetime(overlap_date_list[a], format='%Y-%m-%d')
         modis_date_year = str(modis_date)[0:4]
         new_year_day = pd.Timestamp(year=int(modis_date_year), month=1, day=1)
         doy_temp = str((modis_date - new_year_day).days + 1)
