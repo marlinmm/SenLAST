11b2d9409a3e1ff9e0397f0f12c494be37246ded
marlinmm <marlinmueller.mm@gmail.com>
2020-07-05 17:24:51 +0200

    cleaned up file, added correct MODIS function

--- SenLAST/analysis.py
+++ SenLAST/analysis.py
@@ -13,55 +13,25 @@ def analyze_MODIS_temperature(mod_directory, shape_path):
     returns one temperature array for each station
     """
     import_list = import_polygons(shape_path=shape_path)
-    modis_file_list = extract_files_to_list(path_to_folder=mod_directory)
+    sentinel_file_list = extract_files_to_list(path_to_folder=mod_directory)
+    final_mean = []
+    print("#################### SENTINEL RESULTS ####################")
 
-    print("#################### MODIS RESULTS ####################")
-    Modis_mean_list = []
-    for i, files in enumerate(modis_file_list):
+    for i, files in enumerate(import_list):
         scenes = i+1
-        print(scenes, ". scene")
-        src1 = rio.open(modis_file_list[i])
-        stations_list = []
-        for j, polygons in enumerate(import_list):
+        print(scenes, ". weather station")
+        for j, polygons in enumerate(sentinel_file_list):
             stations = j+1
-            print(stations, ". weather station")
-            mask = rio.mask.mask(src1, [import_list[0][j]], all_touched=True, crop=True, nodata=np.nan)
-            Mod_temperature_array = mask[0][0]
-            mean_Mod = np.nanmean(Mod_temperature_array)
-            stations_list.append(mean_Mod)
-            print(Mod_temperature_array)
-            print(mean_Mod)
-        print(stations_list)
-        Modis_mean_list.append(stations_list)
-    print(np.rot90(np.rot90(np.rot90(Modis_mean_list))))
+            print(stations, ". scene")
+            src1 = rio.open(sentinel_file_list[j])
+            mask = rio.mask.mask(src1, [import_list[0][i]], all_touched=True, crop=True, nodata=np.nan)
+            Sen_temperature_array = mask[0][0]
+            mean_Sen = np.nanmean(Sen_temperature_array)
+            print(Sen_temperature_array)
+            print(mean_Sen)
+            final_mean.append(mean_Sen)
+        print(np.nanmean(final_mean))
 
-# def analyze_SENTINEL_temperature(sen_directory, shape_path):
-#     """
-#     returns the temperature value for each pixel of each station for each SENTINEL scene
-#     prints an array for the stations according to the scenes
-#     :return:
-#     returns three arrays (1st array displayed for each station is the temperature array)
-#     returns one temperature array for each station if a[0][0]
-#     """
-#     import_list = import_polygons(shape_path=shape_path)
-#     sentinel_file_list = extract_files_to_list(path_to_folder=sen_directory)
-#     final_mean = []
-#     print("#################### SENTINEL RESULTS ####################")
-#
-#     for i, files in enumerate(sentinel_file_list):
-#         scenes = i+1
-#         print(scenes, ". scene")
-#         src1 = rio.open(sentinel_file_list[i])
-#         for j, polygons in enumerate(import_list):
-#             stations = j+1
-#             print(stations, ". weather station")
-#             mask = rio.mask.mask(src1, [import_list[0][j]], all_touched=True, crop=True, nodata=np.nan)
-#             Sen_temperature_array = mask[0][0]
-#             mean_Sen = np.nanmean(Sen_temperature_array)
-#             print(Sen_temperature_array)
-#             print(mean_Sen)
-#             final_mean.append(mean_Sen)
-#         print(np.nanmean(final_mean))
 
 def analyze_SENTINEL_temperature(sen_directory, shape_path):
     """
