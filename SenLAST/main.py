93ed5dc4742e405f1548391f4c2173f7b5498791
marlinmm <marlinmueller.mm@gmail.com>
2020-07-05 17:22:24 +0200

    added correct paths for marlin

--- SenLAST/main.py
+++ SenLAST/main.py
@@ -15,32 +15,33 @@ def main():
     ### Jonas base Folder:
     ## MODIS:
     # MODIS_directory = "F:/Test_MODIS/LST/GeoTIFF/Thuringia/scaled/cloud_free"
-    MODIS_directory = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free" # analysis.py, hier wieder final_selected path adden
+    # MODIS_directory = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free" # analysis.py, hier wieder final_selected path adden
     ## Sentinel:
     # Sentinel_directory = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten"
     # Sentinel_directory = "F:/Test_SENTINEL"
-    Sentinel_directory = "F:/Test_SENTINEL/selected/cloud_free" # analysis.py, hier wieder final_selected path adden
+    # Sentinel_directory = "F:/Test_SENTINEL/selected/cloud_free" # analysis.py, hier wieder final_selected path adden
 
     ### Shapefiles:
     ## MODIS Shapefile:
-    MODIS_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
+    # MODIS_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
     ## Sentinel Shapefile:
-    Sentinel_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box.shp"
+    # Sentinel_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box.shp"
 
 
     ### Marlin base Folder:
     ## MODIS:
     # MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download"
-    # MODIS_directory = "F:/411/LST/GeoTIFF/Thuringia/scaled"
+    MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free/final_modis_selected" #analysis.py
     ## Sentinel:
     # Sentinel_directory = "F:/GEO411_data/Sentinel_Daten"
-    # Sentinel_directory = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten/selected/cloud_free/final_sentinel_selected"
+    Sentinel_directory = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free/final_sentinel_selected" #analysis.py
 
     ### Shapefiles:
     ## MODIS Shapefile:
     # MODIS_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
     ## Sentinel Shapefile:
     # Sentinel_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box.shp"
+
 
     ####################### USER-DEPENDENT FUNCTIONS TO BE USED #######################
 
