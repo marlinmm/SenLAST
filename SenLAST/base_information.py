af562553207fca04c8609bd0fe41885cabc8aeb8
marlinmm <marlinmueller.mm@gmail.com>
2020-07-06 14:27:42 +0200

    added extract_MODIS_time_value function to extract the local time stamp from MODIS time_view layer -> needs to be processed and in separate folder structure
    also added renaming function to rename MODIS_LST files with correct time

--- SenLAST/base_information.py
+++ SenLAST/base_information.py
@@ -1,5 +1,7 @@
 import os
 import fiona
+import rasterio as rio
+import numpy as np
 
 
 def extract_files_to_list(path_to_folder):
@@ -16,7 +18,7 @@ def extract_files_to_list(path_to_folder):
             continue
     return new_list
 
-### this function needs to be generalized ###
+
 def import_polygons(shape_path):
     """
     imports the 3x3km polygons of the DWD weather stations
@@ -29,3 +31,51 @@ def import_polygons(shape_path):
         shape_list.append(shapes)
     return shape_list
 
+
+def extract_time_value_MODIS(mod_directory, shape_path):
+    # create new cloud_free directory, overwrite if already exits
+    selected_tifs = extract_files_to_list(path_to_folder=mod_directory)
+
+    # import polygons from shapefile
+    import_list = import_polygons(shape_path=shape_path)
+
+    # loop through all .tif files in folder
+    all_time_list = []
+    for i, tifs in enumerate(selected_tifs):
+        src1 = rio.open(selected_tifs[i])
+        # loop through all weatherstations for each .tif file
+        for j in range(0,1):
+            out_image1, out_transform1 = rio.mask.mask(src1, [import_list[0][j]], all_touched=1, crop=True,
+                                                       nodata=np.nan)
+            temp = np.nanmean(out_image1[0])
+            str_temp = str(temp)
+            result = str_temp.find(".")
+            sol_minute = int(int(str_temp[result+1:result+3]) * 60 / 100)
+            sol_hour = str_temp[result-2:result]
+            normal_hour = int(sol_hour)
+            normal_minute = int(sol_minute) - 46
+            if normal_minute < 0:
+                normal_hour = normal_hour - 1
+                normal_minute = 60 + normal_minute
+                if normal_hour < 10:
+                    normal_hour = "0" + str(normal_hour)
+            if normal_minute < 10:
+                normal_minute = "0" + str(normal_minute)
+            final_normal_time = "___" + str(normal_hour) + "_" + str(normal_minute)
+        all_time_list.append(final_normal_time)
+    return all_time_list
+
+
+
+def rename_files(mod_directory, mod_time_directory,  shape_path):
+    new_mod_time_directory = mod_time_directory + "/final_modis_selected"
+    new_mod_directory = mod_directory + "/final_modis_selected"
+    time_list = extract_time_value_MODIS(mod_directory=new_mod_time_directory, shape_path=shape_path)
+    selected_tifs = extract_files_to_list(path_to_folder=new_mod_directory)
+    for i, tifs in enumerate(selected_tifs):
+        #Marlin Index:
+        name = tifs[0:95]
+        # Jonas Index:
+        #name = tifs[0:hier dein Index]
+        print(name)
+        os.rename(tifs, name + time_list[i] + ".tif")
