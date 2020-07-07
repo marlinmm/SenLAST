9461c5c98da78b8a0258365cceff9d4df66b7aa4
marlinmm <marlinmueller.mm@gmail.com>
2020-07-07 18:02:51 +0200

    more tests for DWD data extraction based on date and time

--- SenLAST/import_dwd_data.py
+++ SenLAST/import_dwd_data.py
@@ -4,7 +4,7 @@ from SenLAST.comparison import extract_SENTINEL_date, extract_SENTINEL_timestamp
 from SenLAST.base_information import import_polygons
 
 #satellite_data = "C:/Users/marli/Google Drive/Studium/Master/2.Semester/GEO411/Praxis/Datenpaare/All/"
-csv_folder = "C:/Users/marli/Google Drive/Studium/Master/2.Semester/GEO411/Praxis/DWD_Daten/379_Bad_Berka/Historisch/"
+csv_folder = "F:/GEO411_data/DWD_result_all/"
 
 MODIS_directory = "F:/GEO411_data/test/small/mod"
 Sentinel_directory = "F:/GEO411_data/test/small/sen"
@@ -23,15 +23,15 @@ print(mod_time)
 
 
 
-# station_list = []
-#
-# with open(csv_folder + "produkt_zehn_min_tu_20170901_20191231_00379.csv", newline='') as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-#     for row in spamreader:
-#         station_list.append(', '.join(row))
-#     print(station_list[2][0:20])
-#     print(len(station_list[2]))
-#
+station_list = []
+
+with open(csv_folder + "combined__2750_Kronach.csv", newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    for row in spamreader:
+        station_list.append(', '.join(row))
+    print(station_list[416])
+    print(len(station_list[2]))
+
 # Sentinel_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box.shp"
 # print(import_polygons(shape_path=Sentinel_shapefile))
 #
