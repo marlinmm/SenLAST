627689acf909d42850e4c79830be517151254db8
jziemer1996 <jz199623@gmail.com>
2020-07-08 12:23:07 +0200

    plotted time series bar chart

--- SenLAST/plots.py
+++ SenLAST/plots.py
@@ -2,12 +2,13 @@ import pandas as pd
 import plotly.express as px
 
 # Jonas Path
-csv_path = "C:/Users/jz199/Desktop/DWD_result_all/"
+# csv_path = "C:/Users/jz199/Desktop/DWD_result_all/"
+csv_path = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/"
 # Marlin Path
 # csv_path = ""
 
 # Filename
-csv_file = "combined__2750_Kronach.csv"
+csv_file = "Datenpaare.csv"
 
 # Concatenation
 csv_data = csv_path + csv_file
@@ -16,8 +17,12 @@ csv_data = csv_path + csv_file
 df = pd.read_csv(csv_data, delimiter=";")
 
 # Print head of csv data
-print(df.head())
+print(df)
 
 # Plot csv data
-fig = px.scatter(df, x = 'TT_10', y = 'TM5_10', title='DWD Data')
+fig = px.bar(df, y='Datenpaare', x='Monat', text='Datenpaare', title='S3/MODIS-Datenpaare (Zeitraum: 07/2018-05/2020)')
+fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
 fig.show()
+
+###################################################################
