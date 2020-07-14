8c08ebaaeb8f930481a50ceccdd7f0cd9d0b2ec5
jziemer1996 <jz199623@gmail.com>
2020-07-14 16:51:58 +0200

    added corr_scatplot_anal.py

--- SenLAST/test_env/main_old.py
+++ SenLAST/test_env/main_old.py
@@ -6,6 +6,8 @@ from SenLAST.comparison import *
 from SenLAST.analysis import *
 from SenLAST.import_dwd_data import *
 from SenLAST.base_information import *
+from SenLAST.plots import *
+
 
 
 start_time = datetime.now()
@@ -19,38 +21,41 @@ def main():
     # MODIS_directory = "F:/Test_MODIS/LST/GeoTIFF/Thuringia/scaled"
     # MODIS_directory = "F:/FINAL_LST_Daten/Datenpaare/2018_07_03/MODIS"
     # MODIS_directory = "F:/411/LST/GeoTIFF/Thuringia/scaled/cloud_free" # analysis.py, hier wieder final_selected path adden
+    # MODIS_Datapair_directory = "F:/411/FINAL_LST_Daten/MODIS_Daten_final_final/MODIS_download/cloud_free"
+    # MODIS_time_directory = "F:/411/FINAL_LST_Daten/MODIS_Daten_final_final/time_layer/cloud_free"
+    MODIS_Datapair_directory = "F:/411/FINAL_LST_Daten/Datenpaare/2018_09_30/MODIS"
     ## Sentinel:
     # Sentinel_directory = "F:/Sentinel_3/S3_Daten-20200622T143538Z-001/S3_Daten"
-    # Sentinel_directory = "F:/Test_SENTINEL"
+    Sentinel_Datapair_directory = "F:/411/FINAL_LST_Daten/Datenpaare/2018_09_30/Sentinel"
     # Sentinel_directory = "F:/Test_SENTINEL/selected/cloud_free" # analysis.py, hier wieder final_selected path adden
     # Sentinel_directory = "F:/FINAL_LST_Daten/Datenpaare/2018_07_03/SENTINEL"
 
     ### Shapefiles:
     ## MODIS Shapefile:
-    # MODIS_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
+    MODIS_Shapefile_directory = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
     ## Sentinel Shapefile:
-    # Sentinel_shapefile = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box.shp"
+    SENTINEL_Shapefile_directory = "C:/Users/jz199/Documents/Studium/Master/2. Semester/Vorlesungsmitschriften/GEO411 - Landschaftsmanagement und Fernerkundung/Auszug_Daten_SandraBauer_MA/Auszug_Daten_SandraBauer_MA/Stationen_Thüringen_Umland_3x3box.shp"
 
 
     ### Marlin base Folder:
     ## MODIS:
     # MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download"
-    MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free"
-    MODIS_time_directory = "F:/GEO411_data/MODIS_Daten/time_layer/cloud_free"
+    # MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free"
+    # MODIS_time_directory = "F:/GEO411_data/MODIS_Daten/time_layer/cloud_free"
     # MODIS_directory = "F:/GEO411_data/MODIS_Daten/MODIS_download/cloud_free/final_modis_selected" #analysis.py
     ## Sentinel:
     # Sentinel_directory = "F:/GEO411_data/Sentinel_Daten"
-    Sentinel_directory = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free"
+    # Sentinel_directory = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free"
     # Sentinel_directory = "F:/GEO411_data/Sentinel_Daten/selected/cloud_free/final_sentinel_selected" #analysis.py
 
     ### Shapefiles:
     ## MODIS Shapefile:
-    MODIS_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
+    # MODIS_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box_reprojected.shp"
     ## Sentinel Shapefile:
     # Sentinel_shapefile = "F:/GEO411_data/Daten_Sandra/new/Stationen_Thüringen_Umland_3x3box.shp"
 
     ### CSV-Data:
-    csv_directory = "F:/GEO411_data/DWD_result_all/"
+    # csv_directory = "F:/GEO411_data/DWD_result_all/"
 
 
     ####################### USER-DEPENDENT FUNCTIONS TO BE USED #######################
@@ -78,9 +83,11 @@ def main():
 
     # rename_files(mod_directory=MODIS_directory, mod_time_directory=MODIS_time_directory, shape_path=MODIS_shapefile)
 
+    mean_diff(mod_directory=MODIS_Datapair_directory, sen_directory=Sentinel_Datapair_directory, daytime_S3="NIGHT",
+             sen_shape_path=SENTINEL_Shapefile_directory, mod_shape_path=MODIS_Shapefile_directory, daytime_MODIS="Night")
 
     ##### EXTRACT DWD DATA #####
-    import_DWD_data_Sentinel(sen_directory=Sentinel_directory, csv_directory=csv_directory)
+    # import_DWD_data_Sentinel(sen_directory=Sentinel_directory, csv_directory=csv_directory)
     #import_DWD_data_MODIS(mod_directory=MODIS_directory, csv_directory=csv_directory)
 
     statistics_time = datetime.now()
