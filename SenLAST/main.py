b13e8f0b052a5c82c7bb604ef7c9860bcdffa56a
marlinmm <marlinmueller.mm@gmail.com>
2020-07-15 10:00:44 +0200

    added new DWD compare functions to main

--- SenLAST/main.py
+++ SenLAST/main.py
@@ -60,9 +60,10 @@ def main():
     #                 sen_shape_path=SENTINEL_Shapefile_directory, mod_shape_path=MODIS_Shapefile_directory,
     #                 daytime_MODIS="Day")
 
-    analyze_MODIS_DWD(path_to_csv=MODIS_DWD_directory, mod_directory=MODIS_cloud_free_directory,
-                      mod_shape_path=MODIS_Shapefile_directory)
-
+    # analyze_MODIS_DWD(path_to_csv=MODIS_DWD_directory, mod_directory=MODIS_cloud_free_directory,
+    #                   mod_shape_path=MODIS_Shapefile_directory, DWD_temp_parameter="TM5_10")
+    analyze_Sentinel_DWD(path_to_csv=SENTINEL_DWD_directory, sen_directory=SENTINEL_cloud_free_directory,
+                         sen_shape_path=SENTINEL_Shapefile_directory, DWD_temp_parameter="TM5_10")
     statistics_time = datetime.now()
     print("extract_files-time = ", statistics_time - start_time, "Hr:min:sec")
 
