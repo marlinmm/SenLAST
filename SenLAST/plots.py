67e81f5d02277d49d7c0908f4d0c511eb6abd8af
marlinmm <marlinmueller.mm@gmail.com>
2020-07-23 17:13:40 +0200

    changed font sizes for plots and added possibility to create boxplots for SenDWD stuff

--- SenLAST/plots.py
+++ SenLAST/plots.py
@@ -99,32 +99,36 @@ def SenMod_DayNight(mod_directory, sen_directory, sen_shape_path, mod_shape_path
                 'Neuhaus a.R.', 'Schmücke', 'Schwarzburg', 'Waltershausen', 'Weimar-S.', 'Olbersleben', 'Krölpa-Rdorf',
                 'Eschwege', 'Hof', 'Kronach', 'Plauen', 'Sontra', 'Lichtentanne']
     fig = go.Figure(data=[
-        go.Bar(name='Sen-3 SLSTR mittlere Tag-Temperatur (°C)', x=stations, y=analyze_SENTINEL_temperature(sen_directory=sen_directory,
+        go.Bar(name='Sen-3 SLSTR mittlere Nacht-Temperatur (°C)', x=stations, y=analyze_SENTINEL_temperature(sen_directory=sen_directory,
                                                                                            sen_shape_path=sen_shape_path,
                                                                                            daytime_S3=daytime_S3)),
-        go.Bar(name='Terra MODIS mittlere Tag-Temperatur (°C)', x=stations, y=analyze_MODIS_temperature(mod_directory=mod_directory,
+        go.Bar(name='Terra MODIS mittlere Nacht-Temperatur (°C)', x=stations, y=analyze_MODIS_temperature(mod_directory=mod_directory,
                                                                                            mod_shape_path=mod_shape_path,
                                                                                            daytime_MODIS=daytime_MODIS))
     ])
     # Change the bar mode
     fig.update_layout(
-        title='Mittlere Tag-Temperatur (n = 23 Szenen)',
-        titlefont_size=28,
+        title='Mittlere Nacht-Temperatur (n = 23 Szenen)',
+        titlefont_size=36,
         xaxis=dict(
             title='Stationen',
-            titlefont_size=20,
-            tickfont_size=14,
+            titlefont_size=32,
+            tickfont_size=26,
         ),
         yaxis=dict(
-            title='Mittlere Tag-Temperatur (°C)',
-            titlefont_size=20,
-            tickfont_size=14,
+            title='Mittlere Nacht-Temperatur (°C)',
+            titlefont_size=32,
+            tickfont_size=28,
         ),
         legend=dict(
-            x=0.865,
-            y=1.0,
+            x=0.82,
+            y=1.05,
             bgcolor='rgba(255, 255, 255, 0)',
-            bordercolor='rgba(255, 255, 255, 0)'
+            bordercolor='rgba(255, 255, 255, 0)',
+            font = dict(
+                size=16,
+                color="black"
+            ),
         ),
         barmode='group',
         bargap=0.15,  # gap between bars of adjacent location coordinates.
@@ -134,10 +138,14 @@ def SenMod_DayNight(mod_directory, sen_directory, sen_shape_path, mod_shape_path
 
 ########################################################################################################################
 
-def mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_path, daytime_S3, daytime_MODIS):
+def mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_path, daytime_S3, daytime_MODIS, path_to_csv):
     diff_list = []
-    a = analyze_SENTINEL_temperature(sen_directory, sen_shape_path, daytime_S3)
-    b = analyze_MODIS_temperature(mod_directory, mod_shape_path, daytime_MODIS)
+    # a = analyze_SENTINEL_temperature(sen_directory, sen_shape_path, daytime_S3)
+    # b = analyze_MODIS_temperature(mod_directory, mod_shape_path, daytime_MODIS)
+    # a = analyze_SENTINEL_temperature(sen_directory, sen_shape_path, daytime_S3)
+    a = analyze_MODIS_temperature(mod_directory, mod_shape_path, daytime_MODIS)
+    DWD_5cm, Sen = analyze_MODIS_DWD(path_to_csv, mod_directory, mod_shape_path, DWD_temp_parameter="TT_10")
+    b = DWD_5cm
     print("Sentinel = ")
     print(a)
     print("MODIS = ")
@@ -150,18 +158,20 @@ def mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_path, dayt
     zip_object = zip(a, b)
     # zip_object = zip(SENTINEL_1d, MODIS_1d)
     for list1_i, list2_i in zip_object:
-        diff_list.append(list1_i - list2_i)
+        diff_list.append(abs(list1_i - list2_i))
+        # diff_list.append(list1_i - list2_i)
     print("Difference S3-MODIS = ")
     print(diff_list)
     print("mean difference = ")
     print(np.mean(diff_list))
-    # print("median difference = ")
-    # print(np.median(diff_list))
+    print("median difference = ")
+    print(np.median(diff_list))
+    print(np.std(diff_list))
 
     return diff_list
 
 
-def barchart_mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_path, daytime_S3, daytime_MODIS):
+def barchart_mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_path, daytime_S3, daytime_MODIS, path_to_csv):
     stations = ['Bad Berka', 'Dachwig', 'Flughafen Erfurt', 'Kleiner Inselberg', 'Bad Lobenstein', 'Martinroda',
                 'Meiningen',
                 'Neuhaus a.R.', 'Schmücke', 'Schwarzburg', 'Waltershausen', 'Weimar-S.', 'Olbersleben', 'Krölpa-Rdorf',
@@ -172,12 +182,12 @@ def barchart_mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_p
         go.Bar(name='', x=stations,
                y=mean_diff(sen_directory=sen_directory, mod_directory=mod_directory,
                                               sen_shape_path=sen_shape_path, mod_shape_path=mod_shape_path,
-                                              daytime_S3=daytime_S3, daytime_MODIS=daytime_MODIS)),
+                                              daytime_S3=daytime_S3, daytime_MODIS=daytime_MODIS, path_to_csv=path_to_csv)),
     ])
     fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
     fig.update_layout(uniformtext_minsize=20, uniformtext_mode='hide')
     fig.update_layout(
-        title='Differenz der mittleren Tag-Temperatur (°C) für jede Station (S3-MODIS)',
+        title='Mittlere Temperaturdifferenz von MODIS zu DWD-Daten (2m) (MODIS-DWD)',
         titlefont_size=30,
         xaxis=dict(
             title='Stationen',
@@ -185,7 +195,7 @@ def barchart_mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_p
             tickfont_size=14,
         ),
         yaxis=dict(
-            title='Differenz der mittleren Tag-Temperatur (°C)',
+            title='Mittlere Temperaturdifferenz von MODIS zu DWD-Daten (2m) (°C)',
             titlefont_size=20,
             tickfont_size=14,
         ),
@@ -206,41 +216,46 @@ def barchart_mean_diff(mod_directory, sen_directory, sen_shape_path, mod_shape_p
 ########################################################################################################################
 
 def SenMod_scatter(mod_directory, sen_directory, sen_shape_path, mod_shape_path, daytime_S3, daytime_MODIS):
-    SENTINEL = analyze_SENTINEL_temperature(sen_directory, sen_shape_path, daytime_S3)
-    MODIS = analyze_MODIS_temperature(mod_directory, mod_shape_path, daytime_MODIS)
+    SENTINEL = extract_Sentinel_temp_list(sen_directory, sen_shape_path, daytime_S3)
+    MODIS = extract_MODIS_temp_list(mod_directory, mod_shape_path, daytime_MODIS)
     print(SENTINEL)
     print(MODIS)
     ### Multiple Means for every station and every scence --> order of scenes is fundamental !!! ###
     SENTINEL_1d = reduce(lambda x,y : x+y, SENTINEL)
     MODIS_1d = reduce(lambda x,y : x+y, MODIS)
     print(SENTINEL_1d)
+    print(len(SENTINEL_1d))
     print(MODIS_1d)
+    print(len(MODIS_1d))
 
-    fig = go.Figure(data=go.Scatter(
-        x=MODIS,
-        y=SENTINEL,
-        mode='markers',
-        marker=dict(
-            color='rgba(187, 67, 141, 1)',
-            size=10,
-            line_width=1
-        ),
-    ))
+    # regression
+    reg = LinearRegression().fit(np.vstack(MODIS_1d), SENTINEL_1d)
+    reg_fit = reg.predict(np.vstack(MODIS_1d))
+
+
+    # fig = go.Figure(data=go.Scatter(x=MODIS_1d, y=SENTINEL_1d, mode='markers',
+    #                                   marker=dict(color='rgba(187, 67, 141, 1)', size=10, line_width=1)))
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(name='line of best fit', x=MODIS_1d, y=reg_fit, mode='lines'))
+    fig.add_trace(go.Scatter(name='X vs Y', x=MODIS_1d, y=SENTINEL_1d, mode='markers'))
+    fig.update_traces(marker=dict(size=10),
+                      selector=dict(mode='markers'))
     fig.update_layout(
-        title='Korrelation der mittleren Tag-Temperatur (MODIS/S3)',
-        titlefont_size=28,
+        title='Korrelation der mittleren Nacht-Temperatur (MODIS/S3)',
+        titlefont_size=32,
         xaxis=dict(
-            title='Mittlere MODIS Tag-Temperatur (°C)',
-            titlefont_size=20,
-            tickfont_size=14,
+            title='Mittlere MODIS Nacht-Temperatur (°C)',
+            titlefont_size=32,
+            tickfont_size=26,
         ),
         yaxis=dict(
-            title='Mittlere SENTINEL Tag-Temperatur (°C)',
-            titlefont_size=20,
-            tickfont_size=14,
+            title='Mittlere SENTINEL Nacht-Temperatur (°C)',
+            titlefont_size=32,
+            tickfont_size=26,
         ))
 
     fig.show()
+
 
 ########################################################################################################################
 
@@ -322,28 +337,31 @@ def plot_MODIS_DWD(path_to_csv, mod_directory, mod_shape_path, DWD_temp_paramete
             DWD_variable = "DWD Temperatur 2m (°C)"
 
         # Fit linear model
-        Mod_DWD_correlation_list = np.array(MOD_data_list[i]).reshape(-1, 1)
-        model = LinearRegression()
-        model.fit(Mod_DWD_correlation_list, temp_2m)
-        model = LinearRegression().fit(Mod_DWD_correlation_list, temp_2m)
-        r_sq = model.score(Mod_DWD_correlation_list, temp_2m)
+        # Mod_DWD_correlation_list = np.array(temp_2m).reshape(-1, 1)
+        # model = LinearRegression()
+        # model.fit(temp_2m, Mod_DWD_correlation_list)
+        # model = LinearRegression().fit(temp_2m, Mod_DWD_correlation_list)
+        # r_sq = model.score(temp_2m, Mod_DWD_correlation_list)
+        #
+        # print("")
+        # print(station_names[i])
+        # print('R2:', r_sq)
+        # intercept = model.intercept_
+        # print('Y-intercept:', model.intercept_)
+        # coefficient = model.coef_
+        # print('Coefficient:', model.coef_)
 
-        print("")
-        print(station_names[i])
-        print('R2:', r_sq)
-        intercept = model.intercept_
-        print('Y-intercept:', model.intercept_)
-        coefficient = model.coef_
-        print('Coefficient:', model.coef_)
+        reg = LinearRegression().fit(np.vstack(MOD_data_list[i]), temp_2m)
+        reg_fit = reg.predict(np.vstack(MOD_data_list[i]))
 
         # Plot scatterplot for each station
         fig, ax = plt.subplots()
         ax.set_title('MODIS/DWD Temperatur Korrelation (' + station_names[i] + ")")
-        ax.set_xlabel('MODIS Temperatur (°C)')
-        ax.set_ylabel(DWD_variable)
-        abline_values = [coefficient * i + intercept for i in Mod_DWD_correlation_list]
-        plt.plot(Mod_DWD_correlation_list, abline_values, ("#FFA500"))
-        plt.plot(MOD_data_list[i], temp_2m, 'o')
+        ax.set_ylabel('MODIS Temperatur (°C)')
+        ax.set_xlabel(DWD_variable)
+        #abline_values = [coefficient * i + intercept for i in Mod_DWD_correlation_list]
+        #plt.plot(reg_fit, temp_2m, ("#FFA500"))
+        plt.plot(temp_2m, MOD_data_list[i], 'o')
         plt.show()
 
 
@@ -352,6 +370,7 @@ def plot_Sentinel_DWD(path_to_csv, sen_directory, sen_shape_path, DWD_temp_param
     Sen_data_list = extract_Sentinel_temp_list(sen_directory=sen_directory, sen_shape_path=sen_shape_path)
     DWD_mean_list = []
     Sen_data_mean_list = []
+    boxplot_result_list = []
     print("######################## SENTINEL ########################")
     for i, file in enumerate(csv_list):
         # Read csv data
@@ -370,6 +389,7 @@ def plot_Sentinel_DWD(path_to_csv, sen_directory, sen_shape_path, DWD_temp_param
 
         # # for bar chart only
         Sen_data_mean_list.append(np.mean(Sen_data_list[i]))
+        boxplot_result_list.append(Sen_data_list[i])
         DWD_mean_list.append(np.mean(temp_2m))
 
         # Fit linear model
@@ -397,6 +417,21 @@ def plot_Sentinel_DWD(path_to_csv, sen_directory, sen_shape_path, DWD_temp_param
         plt.plot(Sen_data_list[i], temp_2m, 'o')
         plt.show()
 
+    #### for creating a boxplot #####
+    # # Create a figure instance
+    # fig = plt.figure(1, figsize=(9, 6))
+    #
+    # # Create an axes instance
+    # ax = fig.add_subplot(111)
+    #
+    # # Create the boxplot
+    # bp = ax.boxplot(boxplot_result_list)
+    # ax.set_xticklabels(station_names, rotation='vertical')
+    # plt.xlabel("Stationen")
+    # plt.ylabel("Mittlere Temp. in °C")
+    #
+    # fig.savefig('fig1.png', bbox_inches='tight')
+
 
 def SenDWD_barchart(sen_directory, sen_shape_path, path_to_csv, DWD_temp_parameter):
     stations = ['Bad Berka', 'Dachwig', 'Flughafen Erfurt', 'Kleiner Inselberg', 'Bad Lobenstein', 'Martinroda', 'Meiningen',
@@ -414,22 +449,26 @@ def SenDWD_barchart(sen_directory, sen_shape_path, path_to_csv, DWD_temp_paramet
     # Change the bar mode
     fig.update_layout(
         title='Mittlere S3/DWD Temperatur (n = 130 Szenen)',
-        titlefont_size=28,
+        titlefont_size=32,
         xaxis=dict(
             title='Stationen',
-            titlefont_size=20,
-            tickfont_size=14,
+            titlefont_size=32,
+            tickfont_size=26,
         ),
         yaxis=dict(
             title='Mittlere Temperatur (°C)',
-            titlefont_size=20,
-            tickfont_size=14,
+            titlefont_size=32,
+            tickfont_size=26,
         ),
         legend=dict(
-            x=0.86,
-            y=1.0,
+            x=0.82,
+            y=1.12,
             bgcolor='rgba(255, 255, 255, 0)',
-            bordercolor='rgba(255, 255, 255, 0)'
+            bordercolor='rgba(255, 255, 255, 0)',
+            font=dict(
+                size=16,
+                color="black"
+            ),
         ),
         barmode='group',
         bargap=0.3,  # gap between bars of adjacent location coordinates.
